8436879e84d6c38abd023dfafc0d59568a89d08d
Denys-bit <denisborisenko479@gmail.com>
2024-06-02 17:23:37 +0300

    Create new class, add TelegramBotClient

---
+++ BoltBot/BotService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace BoltBot
+{
+    internal class BotService
+    {
+        TelegramBotClient botClient = new TelegramBotClient("{7478941975:AAF9nPwf8FWy2033T1XGD35M4Z-dhv4SeUM}");
+    }
+}
